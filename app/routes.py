4b188718af125826731beb34a1bd663ff044f3b3
gylim0604 <gylim0604@hotmail.com>
2020-09-04 20:50:21 -0500

    day2: learned templates

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,18 @@
-from app import app
+from flask import render_template
+from app import app  
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username': 'Bob'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland'
+        },
+        {
+            'author': {'username' : 'Susan'},
+            'body' : 'Cake is nice'
+        }
+    ]
+    return render_template('index.html', title = 'Home', user=user, posts=posts)
